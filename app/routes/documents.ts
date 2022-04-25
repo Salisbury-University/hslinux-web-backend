6fd19684446f8349c828318608465fa72d5895da
dylmcd13 <dylmcd13@gmail.com>
2022-04-25 16:08:59 -0400

    Added single page route and implementation

--- app/routes/documents.ts
+++ app/routes/documents.ts
@@ -1,12 +1,13 @@
-import express from 'express';
-import { DocumentController } from '../http/controllers/DocumentController';
+import express from "express";
+import { DocumentController } from "../http/controllers/DocumentController";
 
 const router = express.Router();
 
 //Router to fetch full list of document ids that the user has access to
-router.get('/api/v1/docs',DocumentController.multiDoc);
+router.get("/api/v1/docs", DocumentController.multiDoc);
 
 //Router to fetch a list of document ids that the user has access to, but in a specific range
-router.get('/api/v1/docs/:page',DocumentController.multiDocPaged);
+router.get("/api/v1/docs/:page", DocumentController.multiDocPaged);
 
+router.get("/api/v1/doc/:id", DocumentController.singleDocPaged);
 export default router;
--- app/services/DocumentService.ts
+++ app/services/DocumentService.ts
@@ -1,92 +1,113 @@
 import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient()
+const { marked } = require("marked");
+const prisma = new PrismaClient();
 
 /**
- * Service for Document that has functions to fetch the documents 
- * from the database 
+ * Service for Document that has functions to fetch the documents
+ * from the database
  */
 export const DocumentService = {
-
+  /**
+   * Grabs all of the documents from the database and sends the document
+   * IDs to the JSON Body
+   * @param req
+   * @param res
+   */
+  async multiDoc(req, res) {
     /**
-     * Grabs all of the documents from the database and sends the document
-     * IDs to the JSON Body
-     * @param req 
-     * @param res 
+     * Gets all the documents from database and sorts them by ascending
      */
-    async multiDoc(req, res){
-
-        /**
-         * Gets all the documents from database and sorts them by ascending
-         */
-        const prismaDocs = await prisma.document.findMany({
-            orderBy: {
-                id: 'asc'
-            }
-        })
-
-        /**
-         * Body to add the document IDs to
-         */
-        const documentList = {
-            docs: []
-        }
-
-        /**
-         * Loops through retrieved documents and pushes Document IDs
-         * into docs property of documentList
-         */
-        for (let i = 0; i < prismaDocs.length; i++) {
-            documentList.docs.push(prismaDocs[i].id)
-        }
-
-        //Send the list of Document IDs to Body
-        res.send(documentList);
-    },
+    const prismaDocs = await prisma.document.findMany({
+      orderBy: {
+        id: "asc",
+      },
+    });
 
     /**
-     * Takes the page number from the request and puts the 10 documents into JSON Body
+     * Body to add the document IDs to
+     */
+    const documentList = {
+      docs: [],
+    };
+
+    /**
+     * Loops through retrieved documents and pushes Document IDs
+     * into docs property of documentList
+     */
+    for (let i = 0; i < prismaDocs.length; i++) {
+      documentList.docs.push(prismaDocs[i].id);
+    }
+
+    //Send the list of Document IDs to Body
+    console.log();
+    res.send(documentList);
+  },
+
+  /**
+   * Takes the page number from the request and puts the 10 documents into JSON Body
+   * Page 1 takes documents 1-10,
+   * Page 2 takes documents 11-20,
+   * etc.
+   * @param req
+   * @param res
+   */
+  async multiDocPaged(req, res) {
+    //Page number from request
+    var page = req.params.page;
+
+    /**
+     * Gets documents from database from a certain page and orders them by ascending
      * Page 1 takes documents 1-10,
      * Page 2 takes documents 11-20,
      * etc.
-     * @param req 
-     * @param res 
      */
-    async multiDocPaged(req, res){
+    const prismaDocs = await prisma.document.findMany({
+      skip: (page - 1) * 10,
+      take: 10,
+      orderBy: {
+        id: "asc",
+      },
+    });
 
-        //Page number from request
-        var page = req.params.page;
+    /**
+     * Body to add the document IDs to
+     */
+    const documents = {
+      docs: [],
+    };
 
-        /**
-         * Gets documents from database from a certain page and orders them by ascending
-         * Page 1 takes documents 1-10,
-         * Page 2 takes documents 11-20,
-         * etc.
-         */
-        const prismaDocs = await prisma.document.findMany({
-            skip: (page-1)*10,
-            take: 10,
-            orderBy: {
-                id: 'asc'
-            }
-        });
-
-        /**
-         * Body to add the document IDs to
-         */
-        const documents = {
-            docs: []
-        }
-
-        /**
-         * Loops through retrieved documents and pushes Document IDs
-         * into docs property of documentList
-         */
-        for (let i = 0; i < prismaDocs.length; i++) {
-            documents.docs.push(prismaDocs[i].id)
-        }
-
-        //Sends list of Document IDs to JSON Body
-        res.send(documents);
+    /**
+     * Loops through retrieved documents and pushes Document IDs
+     * into docs property of documentList
+     */
+    for (let i = 0; i < prismaDocs.length; i++) {
+      documents.docs.push(prismaDocs[i].id);
     }
-}
+
+    //Sends list of Document IDs to JSON Body
+    res.send(documents);
+  },
+
+  async singleDoc(req, res) {
+    var id = req.params.id;
+    const doc = await prisma.document.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    if (!doc) {
+      res.sendStatus(404);
+    } else {
+      res.send({
+        id: id,
+        content: "",
+        metadata: {
+          dateCreated: doc.created,
+          dateUpdated: doc.updatedAt,
+          privilege: doc.group,
+          author: doc.author,
+        },
+      });
+    }
+  },
+};
