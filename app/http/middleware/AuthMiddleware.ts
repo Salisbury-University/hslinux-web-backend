974b429563dd5de7672480340a7197e2b2293b8f
Spencer Evan Lefever <slefever1@gulls.salisbury.edu>
2022-05-04 15:02:15 -0500

    small changes

--- app/http/middleware/AuthMiddleware.ts
+++ app/http/middleware/AuthMiddleware.ts
@@ -1,7 +1,5 @@
 import { NextFunction, Request, Response } from "express";
 import { AuthService } from "../../services/AuthService";
-import UnauthorizedException from "../../exceptions/UnauthorizedException";
-import JWTMalformedException from "../../exceptions/JWTMalformedException";
 import { nextTick } from "process";
 
 
