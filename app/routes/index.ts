3ee4c85378b2dc5b3558cf999975dd02f620c964
dylmcd13 <dylmcd13@gmail.com>
2022-09-09 13:01:15 -0400

    Moved document routes to index.ts
    Probably delete documents.ts in routes folder since it's not used now

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -19,10 +19,20 @@ router.get("/", IndexController.index);
 // Example route for input validation that expects a JSON with a message string
 router.post("/", validate(schema), IndexController.index);
 
-
 // Nested router for authentication examples
 import authRouter from "./auth";
 router.use("/auth", authRouter);
+
+import { DocumentController } from '../http/controllers/DocumentController'
+//Router to fetch full list of document ids that the user has access to
+router.get("/api/v1/docs", DocumentController.multiDoc);
+
+//Router to fetch a list of document ids that the user has access to, but in a specific range
+router.get("/api/v1/docs/:page", DocumentController.multiDocPaged);
+
+//Router for fetching a single document
+router.get("/api/v1/doc/:id", DocumentController.singleDoc);
+
 
 import documentRouter from "./documents"
 router.use(documentRouter);
