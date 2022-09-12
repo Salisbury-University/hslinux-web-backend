21d66782ae8cc06d1558adfb977e44e99593b128
dylmcd13 <dylmcd13@gmail.com>
2022-09-12 13:36:21 -0400

    Changed File Name of exceptions so they are like master branch

---
+++ app/exceptions/BadRequestException.ts
@@ -0,0 +1,14 @@
+import BaseException from "./BaseException";
+
+/**
+ * On route '/api/v1/docs/:page'
+ * When a page is less than 1 or if the page contains letters, throws PageException
+ * 
+ * Sets message to 'Bad Request' and set HTTP Status to 400
+ */
+export default class BadRequestException extends BaseException {
+  message: string;
+  constructor(message: string = "Bad Request") {
+    super(message, 400);
+  }
+}
