71ffdbda6765decc641c3f228e17a600ff8ba67c
dylmcd13 <dylmcd13@gmail.com>
2022-09-23 12:47:34 -0400

    Changed comments

--- app/exceptions/NotFoundException.ts
+++ app/exceptions/NotFoundException.ts
@@ -3,7 +3,7 @@ import BaseException from "./BaseException";
 /**
  * Exception class tailored to 404 Not Found exceptions.
  *
- * Contains a default error message and sets the HTTP response status.
+ * Contains a default error message and sets the HTTP response status to 404.
  */
 export default class NotFoundException extends BaseException {
   constructor(message: string = "Resource Not Found") {
