bc973b45ffe46b0aa039a7ee9713ebb2a34aec00
Spencer Evan Lefever <slefever1@gulls.salisbury.edu>
2022-10-03 11:53:57 -0400

    Update .gitignore

--- app/services/DocumentService.ts
+++ app/services/DocumentService.ts
@@ -149,8 +149,10 @@ export function parseFrontmatter() {
 
         const frontMatter = token[1].text.split("\n");
 
-        console.log(frontMatter);
+        console.log(typeof(frontMatter));
+        console.log(frontMatter[0]);
         
+
         /** TODO
          * Remove console.log
          * Access data without frontmatter
