c5d9e51be36f1bf8af6dba440dab013f91acf1ff
dylmcd13 <dylmcd13@gmail.com>
2022-10-21 13:15:50 -0400

    Fixed controller unit tests

--- tests/DocumentController.spec.ts
+++ tests/DocumentController.spec.ts
@@ -7,9 +7,9 @@ test.group("Document Controller", () => {
   test("multiDoc is returned content", async ({ expect }, done: Function) => {
     request(app)
       .get("/api/v1/docs/")
-      .send({"uid": "Dylan"})
+      .send({"uid": "Alice"})
       .then(( {body} ) => {
-        expect(body.docs).toBeDefined();
+        expect(body.docs.length).toBeGreaterThan(0)
         done();
       });
   }).waitForDone();
@@ -17,17 +17,19 @@ test.group("Document Controller", () => {
   test("multiDocPaged is returned content", async ({ expect }, done: Function) => {
     request(app)
       .get("/api/v1/docs/1")
+      .send({"uid": "Alice"})
       .then(( {body} ) => {
-        expect(body.docs).toBeDefined();
+        expect(body.docs.length).toBeGreaterThan(0);
         done();
       });
   }).waitForDone();
 
   test("singleDoc is returned content", async ({ expect }, done: Function) => {
     request(app)
-      .get("/api/v1/doc/test")
+      .get("/api/v1/doc/facultyTest")
+      .send({"uid": "Alice"})
       .then(( {body} ) => {
-        expect(body).toBeDefined();
+        expect(body.id).toBe("facultyTest")
         done();
       });
   }).waitForDone();
